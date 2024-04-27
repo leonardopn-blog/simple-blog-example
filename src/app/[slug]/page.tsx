ee3940109a1fb34971461c1f8fa17f2a2462f97a
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-04-26 21:40:48 -0300

    feat: :sparkles: Implement the post page

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,49 @@
+import { createClient } from "@/prismicio";
+import { asText } from "@prismicio/client";
+import { PrismicImage, PrismicRichText } from "@prismicio/react";
+import dayjs from "dayjs";
+import { CircleArrowLeft } from "lucide-react";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import "./styles.scss";
+
+interface BlogPostProps {
+	params: {
+		slug: string;
+	};
+}
+
+export default async function BlogPost({ params }: BlogPostProps) {
+	const prismicClient = createClient();
+	const post = await prismicClient.getByUID("blog_post", params.slug).catch(() => notFound());
+
+	return (
+		<div className="flex flex-col gap-4" id="blogPost">
+			<header className="flex flex-col gap-4">
+				<Link
+					href=".."
+					className="hover:opacity-80 transition-opacity flex gap-2 items-center mb-5">
+					<CircleArrowLeft /> Voltar
+				</Link>
+				<h1 className="text-3xl font-bold">{asText(post.data.title)}</h1>
+				<h2 className="text-xl text-gray-500">{asText(post.data.subtitle)}</h2>
+				<PrismicImage field={post.data.banner} />
+				<section>
+					{post.data.author} - Criado em{" "}
+					{dayjs(post.first_publication_date).format("DD/MM/YYYY")}
+				</section>
+			</header>
+			<hr />
+			<main className="flex flex-col gap-4 dark:text-gray-400 text-gray-600 font-light">
+				{post.data.content.map((item, index) => (
+					<section key={index}>
+						<h3 className="text-2xl font-medium text-slate-900 dark:text-white">
+							{asText(item.title)}
+						</h3>
+						<PrismicRichText field={item.body} />
+					</section>
+				))}
+			</main>
+		</div>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,20 @@ export default async function Component() {
 	const posts = await prismicClient.getAllByType("blog_post");
 
 	return (
-		<section className="px-4 py-6 md:px-6 md:py-12 lg:py-16 dark:bg-gray-900 min-h-screen">
-			<div className="grid max-w-3xl gap-6 mx-auto lg:gap-8 lg:max-w-5xl">
-				<div className="space-y-2">
-					<h2 className="text-3xl font-bold tracking-tight sm:text-4xl text-slate-900 dark:text-white">
-						Meu Blog
-					</h2>
-					<p className="text-gray-500 dark:text-gray-400">
-						Blog onde vou escrever sobre tutoriais e dicas de programação.
-					</p>
-				</div>
-				<div className="flex flex-col divide-y">
-					{posts.map(post => (
-						<PostItem key={post.id} post={post} />
-					))}
-				</div>
+		<div className="grid gap-6 lg:gap-8">
+			<div className="space-y-2">
+				<h2 className="text-3xl font-bold tracking-tight sm:text-4xl text-slate-900 dark:text-white">
+					Meu Blog
+				</h2>
+				<p className="text-gray-500 dark:text-gray-400">
+					Blog onde vou escrever sobre tutoriais e dicas de programação.
+				</p>
 			</div>
-		</section>
+			<div className="flex flex-col divide-y">
+				{posts.map(post => (
+					<PostItem key={post.id} post={post} />
+				))}
+			</div>
+		</div>
 	);
 }
